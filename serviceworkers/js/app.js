17b9e7c49e8fa52cd4bf5f2c63228f6fbcae6cbc
Maxime Verleye <mav@emakina.com>
2016-07-07 11:37:39 +0200

    disable services workers

--- serviceworkers/js/app.js
+++ serviceworkers/js/app.js
@@ -9,17 +9,17 @@ define([
 	(function(){
 
 		// Add feature check for Service Workers here
-		if('serviceWorker' in navigator) {
-			navigator.serviceWorker
-				.register('/service-worker.js')
-				.then(
-					function() {
-						console.log('Service Worker Registered');
-					},
-					function(){
-						console.log('Error Service workers');
-					});
-		}
+		// if('serviceWorker' in navigator) {
+		// 	navigator.serviceWorker
+		// 		.register('/service-worker.js')
+		// 		.then(
+		// 			function() {
+		// 				console.log('Service Worker Registered');
+		// 			},
+		// 			function(){
+		// 				console.log('Error Service workers');
+		// 			});
+		// }
 
 		window.App = new Router();
 		Backbone.history.start();
