cfa86c4c0fd1e78cc34bce778af1c5d15966ac42
Maxime Verleye <mav@emakina.com>
2016-07-07 11:56:39 +0200

    enable service workers

--- serviceworkers/js/app.js
+++ serviceworkers/js/app.js
@@ -8,18 +8,18 @@ define([
 ], function ($, Backbone, Router) {
 	(function(){
 
-		// Add feature check for Service Workers here
-		// if('serviceWorker' in navigator) {
-		// 	navigator.serviceWorker
-		// 		.register('/service-worker.js')
-		// 		.then(
-		// 			function() {
-		// 				console.log('Service Worker Registered');
-		// 			},
-		// 			function(){
-		// 				console.log('Error Service workers');
-		// 			});
-		// }
+		/* Add feature check for Service Workers here */
+		if('serviceWorker' in navigator) {
+			navigator.serviceWorker
+				.register('/serviceworkers/service-worker.js')
+				.then(
+					function() {
+						console.log('Service Worker Registered');
+					},
+					function(){
+						console.log('Error Service workers');
+					});
+		}
 
 		window.App = new Router();
 		Backbone.history.start();
